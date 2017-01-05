c20f039677080aafa789fc023fbb10f769f16f7e
Unknown <melanie.s@codeur.online>
2017-01-05 13:40:45 +0100

    integration JQ

---
+++ JS/fichier.js
@@ -0,0 +1,12 @@
+$(function()
+{
+    setInterval(function()
+    {
+         $("#slider ul").animate({marginLeft:-350},800,function()
+         {
+            $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+         })
+
+    }, 3500);
+
+});
