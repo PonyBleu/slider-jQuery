9fc2ff4f1c19aca0bc3d913f64f0caa068bda7e9
Unknown <melanie.s@codeur.online>
2017-01-05 14:38:09 +0100

    ajustement slider

--- JS/fichier.js
+++ JS/fichier.js
@@ -1,12 +1,17 @@
+/*----------------------------slider auto-----------------------------------*/
+
 $(function()
 {
     setInterval(function()
     {
-         $("#slider ul").animate({marginLeft:-350},800,function()
+         $("#slider ul").animate({marginLeft:-430},function()
          {
             $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
          })
 
-    }, 3500);
+    }, 2000);
 
 });
+
+/*--------------------------------------------------------------------------*/
+
