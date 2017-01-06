164e87161bb810736830ecf253953a821f13c92f
Unknown <melanie.s@codeur.online>
2017-01-06 09:47:50 +0100

    insertion commande bouton

--- JS/fichier.js
+++ JS/fichier.js
@@ -1,17 +1,45 @@
 /*----------------------------slider auto-----------------------------------*/
-
-$(function()
-{
-    setInterval(function()
+ 
+var interval =  setInterval(function()
     {
-         $("#slider ul").animate({marginLeft:-430},function()
-         {
-            $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
-         })
-
+        
+        $("#slider ul").animate({marginLeft:-430},function()
+           {
+              $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+            })
+  
     }, 2000);
+
+
+ 
+/*--------------------------------------------------------------------------*/
+
+
+/*------------------------------------button Next-----------------------------------*/
+
+$(".next").click(function () 
+{
+    clearInterval(interval);
+    $("#slider ul").animate({marginLeft:-430},function()
+           {
+              $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+            })
 
 });
 
 /*--------------------------------------------------------------------------*/
 
+
+/*------------------------------------button Back-----------------------------------*/
+
+$(".back").click(function () 
+{
+    clearInterval(interval);
+    $("#slider ul").animate({marginRight:-430},function()
+           {
+              $(this).css({marginRight:0}).find("li:last").after($(this).find("li:first"));
+            })
+
+});
+
+/*--------------------------------------------------------------------------*/
